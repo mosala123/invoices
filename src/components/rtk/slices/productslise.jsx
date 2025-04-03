3123d0360edb6db563e0bac8afb07d2afee2525e
library <elmosalah74@gmail.com>
2025-04-03 14:32:48 +0200

    updete

--- src/components/rtk/slices/productslise.jsx
+++ src/components/rtk/slices/productslise.jsx
@@ -1,7 +1,11 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 export const fetchProduct = createAsyncThunk("productSlice/fetchProduct", async () => {
-    const res = await fetch("https://www.freetestapi.com/api/v1/users");
+    const res = await fetch(
+        `https://api.allorigins.win/raw?url=${encodeURIComponent(
+          "https://www.freetestapi.com/api/v1/users"
+        )}`
+      );
     const data = await res.json();
     return data;
 });
